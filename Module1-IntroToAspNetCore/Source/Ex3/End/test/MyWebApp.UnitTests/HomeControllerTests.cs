33899af6a8e5483a01608edfd610eac45da9d4f5
Jon Galloway <jongalloway@gmail.com>
2016-03-22 19:34:55 -0700

    Added HomeController tests

---
+++ Module1-IntroToAspNetCore/Source/Ex3/End/test/MyWebApp.UnitTests/HomeControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Mvc;
+using MyWebApp.Controllers;
+using Xunit;
+
+namespace MyWebApp.UnitTests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void HomeControllerIndexReturnsResponse() {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
